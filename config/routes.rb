e81ef5433eccade2272b242de6c47e603edef38f
Hugalito <hugo.rischmann@me.com>
2023-06-12 16:18:04 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/index'
   root to: "static_pages/index"
+  
   resources :locations
   resources :plants
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
